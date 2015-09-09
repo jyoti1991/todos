fddc728a6b677f6265f2a1e1e280509e0f30ef79
Ranjana Choudhary <rockingrails@gmail.com>
2015-09-09 16:31:18 +0530

    small changes

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -2,6 +2,6 @@ class Todo < ActiveRecord::Base
 	validates :Name,:Description,:Price, presence: true,
                     length: { minimum: 3 }
 	 has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/pic.jpg"
-  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :image,presence: true, :content_type => /\Aimage\/.*\Z/
   belongs_to :user
 end
